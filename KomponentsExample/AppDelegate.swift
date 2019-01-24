31e28e7ac513d82fb6f6a208c8ae038ebcbd9beb
S4cha <sachadso@gmail.com>
2019-01-24 17:05:21 +0100

    Add injection for Xcode snippet

--- KomponentsExample/AppDelegate.swift
+++ KomponentsExample/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/iOSInjection10.bundle")?.load()
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         let navVC = UINavigationController(rootViewController: NavigationVC())
         navVC.navigationBar.isTranslucent = false
